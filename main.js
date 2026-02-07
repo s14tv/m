3ca810289f3db2d60250b6ec1293aa90f08b8f9c
s14tv <Eyadh9091@gmail.com>
2026-02-07 18:04:46 +0200

    Add navigation functions for page redirection

---
+++ main.js
@@ -0,0 +1,11 @@
+function goYes() {
+  window.location.href = "birthday.html";
+}
+
+function goNo() {
+  window.location.href = "not-you.html";
+}
+
+function next() {
+  window.location.href = "love.html";
+}

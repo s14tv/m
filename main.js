9255515bb03e9b5b770602232438586aa15be475
s14tv <Eyadh9091@gmail.com>
2026-02-07 18:15:19 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,11 @@
+function goYes() {
+  window.location.href = "brithday.html";
+}
+
+function goNo() {
+  window.location.href = "not-you.html";
+}
+
+function next() {
+  window.location.href = "love.html";
+}
